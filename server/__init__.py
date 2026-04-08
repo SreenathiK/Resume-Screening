1e48620c0e7fbf82bd9855e40c91221a08ea5c48
SreenathiK <218193332+SreenathiK@users.noreply.github.com>
2026-04-08 22:53:03 +0530

    Remove openenv dependency from environment

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,4 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-"""Resume Screening environment server components."""
-
+# server/__init__.py
 from server.my_env_environment import ResumeScreeningEnvironment, TaskDifficulty
 
 __all__ = ["ResumeScreeningEnvironment", "TaskDifficulty"]
